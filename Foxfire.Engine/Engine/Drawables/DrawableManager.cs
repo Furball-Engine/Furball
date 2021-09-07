2a3b4d39184966875e2f52a6715a0c81d1cb1f21
Eevee <developerbrzoza@gmail.com>
2021-09-07 13:33:15 +0200

    implemented updating

--- Foxfire.Engine/Engine/Drawables/DrawableManager.cs
+++ Foxfire.Engine/Engine/Drawables/DrawableManager.cs
@@ -4,8 +4,8 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Foxfire.Engine.Engine.Drawables {
     public class DrawableManager {
-        private List<UnmanagedDrawable> _unmanagedDrawables  = new();
-        private List<ManagedDrawable>   _managedDrawables  = new();
+        private List<UnmanagedDrawable> _unmanagedDrawables = new();
+        private List<ManagedDrawable>   _managedDrawables   = new();
 
         public void Draw(GameTime time, SpriteBatch batch) {
             batch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
@@ -21,10 +21,28 @@ namespace Foxfire.Engine.Engine.Drawables {
             batch.End();
 
             lock (this._unmanagedDrawables) {
-                for (int i = 0; i != this._unmanagedDrawables.Count) {
+                for (int i = 0; i != this._unmanagedDrawables.Count; i++) {
                     UnmanagedDrawable currentDrawable = this._unmanagedDrawables[i];
 
                     currentDrawable.Draw(time, batch);
+                }
+            }
+        }
+
+        public void Update(GameTime time) {
+            lock (this._managedDrawables) {
+                for (int i = 0; i != this._managedDrawables.Count; i++) {
+                    ManagedDrawable currentDrawable = this._managedDrawables[i];
+
+                    currentDrawable.Update(time);
+                }
+            }
+
+            lock (this._unmanagedDrawables) {
+                for (int i = 0; i != this._unmanagedDrawables.Count; i++) {
+                    UnmanagedDrawable currentDrawable = this._unmanagedDrawables[i];
+
+                    currentDrawable.Update(time);
                 }
             }
         }
--- Foxfire.Engine/FoxfireGame.cs
+++ Foxfire.Engine/FoxfireGame.cs
@@ -14,30 +14,22 @@ namespace Foxfire.Engine {
         }
 
         protected override void Initialize() {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
