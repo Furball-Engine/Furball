d34beb71900537009a2bafd3b1af6442ad42d464
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-09 15:22:35 -0700

    Prevent unnessesary Object allocation

--- Furball.Engine/Engine/Input/InputMethods/MonogameMouseInputMethod.cs
+++ Furball.Engine/Engine/Input/InputMethods/MonogameMouseInputMethod.cs
@@ -5,7 +5,7 @@ namespace Furball.Engine.Engine.Input.InputMethods {
 	public class MonogameMouseInputMethod : InputMethod {
 		public override void Update() {
 			MouseStateXna currentMouseState = Mouse.GetState(FurballGame.Instance.Window);
-			this.CursorPositions[0] = new MouseState(currentMouseState, "MonogameMouse");
+			this.CursorPositions[0].State = currentMouseState;
 		}
 		public override void Dispose() { }
 		public override void Initialize() {
