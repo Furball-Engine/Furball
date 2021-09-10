36369cde091457ab1600b62d36a59ed7fb42a396
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-09 19:42:28 -0700

    Implement some helpers to help with platform specific code

---
+++ Furball.Engine/Engine/Platform/Linux/Library.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Furball.Engine.Engine.Platform.Linux {
+	public class Library {
+		/// <summary>
+		///     A list of flags available to load a library with
+		/// </summary>
+		[Flags]
+		public enum LoadFlags {
+			RTLD_LAZY         = 0x00001,
+			RTLD_NOW          = 0x00002,
+			RTLD_BINDING_MASK = 0x00003,
+			RTLD_NOLOAD       = 0x00004,
+			RTLD_DEEPBIND     = 0x00008,
+			RTLD_GLOBAL       = 0x00100,
+			RTLD_LOCAL        = 0x00000,
+			RTLD_NODELETE     = 0x01000
+		}
+
+		[DllImport("libdl.so", EntryPoint = "dlopen")]
+		private static extern IntPtr dlopen(string library, LoadFlags flags);
+
+		/// <summary>
+		///     Loads a native Linux library
+		/// </summary>
+		/// <param name="filename">The filename of the library to load</param>
+		/// <param name="flags">The flags of the load</param>
+		public static void Load(string filename, LoadFlags flags) {
+			dlopen(filename, flags);
+		}
+	}
+}
---
+++ Furball.Engine/Engine/Platform/RuntimeInfo.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices;
+
+namespace Furball.Engine.Engine.Platform {
+	public static class RuntimeInfo {
+		public static OSPlatform CurrentPlatform() {
+			if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				return OSPlatform.Windows;
+			if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				return OSPlatform.Linux;
+			if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+				return OSPlatform.OSX;
+			if(RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+				return OSPlatform.FreeBSD;
+			return OSPlatform.Windows;
+		}
+	}
+}
