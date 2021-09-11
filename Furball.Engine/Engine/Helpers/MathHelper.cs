45cf5447f2e81deec6f7fd65e847a0c52fb82be5
Eevee <developerbrzoza@gmail.com>
2021-09-11 22:48:12 +0200

    Helpers: Basic Degrees to Radians converter

--- Furball.Engine/Engine/Helpers/MathHelper.cs
+++ Furball.Engine/Engine/Helpers/MathHelper.cs
@@ -1,6 +1,10 @@
+using System;
+
 namespace Furball.Engine.Engine.Helpers {
     public static class MathHelper {
         //I wish i knew how lerp worked to explain this
         public static double Lerp(double start, double end, double amount) => start + (end - start) * amount;
+
+        public static double DegreesToRadians(double deg) => deg * Math.PI / 180.0;
     }
 }
--- Furball.Game/Screens/BasicTestScreen.cs
+++ Furball.Game/Screens/BasicTestScreen.cs
@@ -8,6 +8,7 @@ using Furball.Engine.Engine.Graphics.Drawables.Tweens;
 using Furball.Engine.Engine.Graphics.Drawables.Tweens.TweenTypes;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MathHelper=Furball.Engine.Engine.Helpers.MathHelper;
 
 namespace Furball.Game.Screens {
     public class BasicTestScreen : Screen {
@@ -16,9 +17,9 @@ namespace Furball.Game.Screens {
 
             whiteTexture.Tweens.Add(new VectorTween(TweenType.Movement, new Vector2(240, 240), new Vector2(540,   540),   1000, 10000, Easing.In));
             whiteTexture.Tweens.Add(new VectorTween(TweenType.Scale,    new Vector2(1,   1),   new Vector2(0.25f, 0.25f), 1000, 10000, Easing.In));
-            whiteTexture.Tweens.Add(new FloatTween(TweenType.Rotation, 0f, (float)Math.PI * 8f, 1000, 10000));
-            whiteTexture.Tweens.Add(new FloatTween(TweenType.Fade,     1f, 0.5f,               1000, 10000));
-            whiteTexture.Tweens.Add(new ColorTween(TweenType.Color, Color.White, Color.Red, 1000, 10000));
+            whiteTexture.Tweens.Add(new FloatTween(TweenType.Rotation, 0f, (float)MathHelper.DegreesToRadians(1440), 1000, 10000, Easing.None));
+            whiteTexture.Tweens.Add(new FloatTween(TweenType.Fade,     1f, 0.5f,               1000, 10000, Easing.None));
+            whiteTexture.Tweens.Add(new ColorTween(TweenType.Color, Color.White, Color.Red, 1000, 10000, Easing.None));
 
             this.Manager.Add(whiteTexture);
 
