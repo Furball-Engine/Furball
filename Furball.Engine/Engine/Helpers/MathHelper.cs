65756dd972c838b10fc6d9729f43feb41d7eb7bd
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-12 17:38:05 -0700

    Helpers: Document DegreesToRadians

--- Furball.Engine/Engine/Helpers/MathHelper.cs
+++ Furball.Engine/Engine/Helpers/MathHelper.cs
@@ -4,7 +4,12 @@ namespace Furball.Engine.Engine.Helpers {
     public static class MathHelper {
         //I wish i knew how lerp worked to explain this
         public static double Lerp(double start, double end, double amount) => start + (end - start) * amount;
-
+        
+        /// <summary>
+        /// Convert a rotation amount in degrees to radians 
+        /// </summary>
+        /// <param name="deg">Amount in degrees</param>
+        /// <returns>Amount in radians</returns>
         public static double DegreesToRadians(double deg) => deg * Math.PI / 180.0;
     }
 }
