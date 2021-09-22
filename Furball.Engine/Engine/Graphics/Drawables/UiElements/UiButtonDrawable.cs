c42be8ae5ebe65ab289a8cc569cfc269042f801e
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-21 18:22:32 -0700

    UiElements: Add a button UI element for a button

---
+++ Furball.Engine/Engine/Graphics/Drawables/UiElements/UiButtonDrawable.cs
@@ -0,0 +1,52 @@
+using Furball.Engine.Engine.Graphics.Drawables.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace Furball.Engine.Engine.Graphics.Drawables.UiElements {
+    public class UiButtonDrawable : ManagedDrawable {
+        private string _text;
+        public string Text => this._text;
+        public TextDrawable TextDrawable;
+
+        private float _margin;
+        public float Margin => this._margin;
+
+        public Color OutlineColor = Color.Black;
+
+        public UiButtonDrawable(string text, byte[] font, float size, Color textColor, Color outlineColor, float margin = 5f) {
+            this.TextDrawable = new TextDrawable(font, text, size);
+
+            this.OnMove += this.RecalculateSize;
+
+            this._margin = margin;
+            this._text   = text;
+
+            this.Size = new Vector2();
+
+            this.RecalculateSize(this, this.Position);
+
+            this.TextDrawable.ColorOverride = textColor;
+            this.OutlineColor               = outlineColor;
+        }
+
+        private void RecalculateSize(object sender, Vector2 newPos) {
+            (float textDrawableSizeX, float textDrawableSizeY) = this.TextDrawable.Size;
+
+            this.Size = new Vector2(textDrawableSizeX + this._margin * 2f, textDrawableSizeY + this._margin * 2f);
+        }
+
+        public override void Draw(GameTime time, SpriteBatch batch, DrawableManagerArgs args) {
+            batch.FillRectangle(args.Position, this.Size, args.Color);
+            batch.DrawRectangle(args.Position, this.Size, this.OutlineColor, 1f, args.LayerDepth);
+
+            // FIXME: this is a bit of a hack, it should definitely be done differently
+            DrawableManagerArgs tempArgs = args;
+            tempArgs.Position.X += this._margin * args.Scale.X;
+            tempArgs.Position.Y += this._margin * args.Scale.Y;
+            tempArgs.Color      =  this.TextDrawable.ColorOverride;
+            tempArgs.Scale      /= (float)FurballGame.Instance.GraphicsDevice.Viewport.Height / FurballGame.DEFAULT_WINDOW_HEIGHT;
+            this.TextDrawable.Draw(time, batch, tempArgs);
+        }
+    }
+}
--- Furball.Game/Screens/BasicTestScreen.cs
+++ Furball.Game/Screens/BasicTestScreen.cs
@@ -1,11 +1,14 @@
 using System;
+using Furball.Engine;
 using Furball.Engine.Engine;
 using Furball.Engine.Engine.Graphics;
 using Furball.Engine.Engine.Graphics.Drawables;
 using Furball.Engine.Engine.Graphics.Drawables.Tweens;
+using Furball.Engine.Engine.Graphics.Drawables.UiElements;
 using Furball.Engine.Engine.Graphics.Drawables.Tweens.TweenTypes;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
 using MathHelper=Furball.Engine.Engine.Helpers.MathHelper;
 
 namespace Furball.Game.Screens {
@@ -27,6 +30,37 @@ namespace Furball.Game.Screens {
             };
 
             this.Manager.Add(whiteTexture);
+
+            UiButtonDrawable testButton = new("test button", ContentReader.LoadRawAsset("default-font.ttf"), 50, Color.White, Color.Black) {
+                Position      = new Vector2(200, 200),
+                ColorOverride = Color.Blue
+            };
+            testButton.OnHover += delegate {
+                testButton.Tweens.Add(
+                new VectorTween(
+                TweenType.Movement,
+                testButton.Position,
+                new(testButton.Position.X + FurballGame.Random.NextSingle(-50, 50), testButton.Position.Y + FurballGame.Random.NextSingle(-50, 50)),
+                FurballGame.Time,
+                FurballGame.Time + 1000
+                )
+                );
+            };
+            testButton.OnUnHover += delegate {
+                testButton.Tweens.Add(
+                new VectorTween(
+                TweenType.Movement,
+                testButton.Position,
+                new(testButton.Position.X + FurballGame.Random.NextSingle(-50, 50), testButton.Position.Y + FurballGame.Random.NextSingle(-50, 50)),
+                FurballGame.Time,
+                FurballGame.Time + 1000
+                )
+                );
+            };
+            testButton.OnClick += delegate {
+                Console.WriteLine("clicked!");
+            };
+            this.Manager.Add(testButton);
 
             //AudioStream stream = AudioEngine.LoadFile("testaudio.mp3");
             //stream.Play();
