df0b32b73cd33b1816ddf0a669fc9f2b782a0dcd
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-23 19:00:25 -0700

    Helpers: Document how our lerp algorithm works

--- Furball.Engine/Engine/Helpers/MathHelper.cs
+++ Furball.Engine/Engine/Helpers/MathHelper.cs
@@ -3,6 +3,24 @@ using System;
 namespace Furball.Engine.Engine.Helpers {
     public static class MathHelper {
         //I wish i knew how lerp worked to explain this
+        //i know tho >:3 -beyley
+        /// <summary>
+        /// Takes a linear interpolation of 2 values and an amount, the algorithm works as follows<br></br>
+        ///
+        /// The goal of this algorithm is to have
+        /// <code>amount=0.0 == start</code>
+        /// and
+        /// <code>amount=1.0 == end</code>
+        /// To be able to achive this we need to be able to change start by at least the difference of end and start,
+        /// as start plus the difference of end and start is equal to end<br></br><br></br>
+        /// Due to <code>start + (difference) == end</code>
+        /// Adding half of difference to start should be half of the way to end from start, 1/4th of difference is 1/4th the way to end from start,
+        /// which is the lerped value we want 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="amount"></param>
+        /// <returns>The interpolated value</returns>
         public static double Lerp(double start, double end, double amount) => start + (end - start) * amount;
         
         /// <summary>
