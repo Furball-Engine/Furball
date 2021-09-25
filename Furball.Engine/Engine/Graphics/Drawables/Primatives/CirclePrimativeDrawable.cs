b0674b73bfb44b1aace9b16c664c0374a8e29dd1
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-25 01:20:41 -0700

    Graphics: Add CirclePrimativeDrawable This is just a basic circle renderer

---
+++ Furball.Engine/Engine/Graphics/Drawables/Primatives/CirclePrimativeDrawable.cs
@@ -0,0 +1,22 @@
+using Furball.Engine.Engine.Graphics.Drawables.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace Furball.Engine.Engine.Graphics.Drawables.Primatives {
+    public class CirclePrimativeDrawable : ManagedDrawable {
+        public float Radius;
+        public float Thickness = 1f;
+        public int   Sides     = 10;
+
+        public CirclePrimativeDrawable(float radius, float thickness, Color color) {
+            this.Radius        = radius;
+            this.Thickness     = thickness;
+            this.ColorOverride = color;
+        }
+
+        public override void Draw(GameTime time, SpriteBatch batch, DrawableManagerArgs args) {
+            batch.DrawCircle(args.Position, this.Radius, this.Sides, args.Color, this.Thickness, args.LayerDepth);
+        }
+    }
+}
