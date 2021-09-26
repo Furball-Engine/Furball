04525f139912d29b853f2d685d3bea3b148ecfd5
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-25 19:38:57 -0700

    Graphics: Implement line and rectangle primitives

--- Furball.Engine/Engine/Graphics/Drawables/Primitives/LinePrimitiveDrawable.cs
+++ Furball.Engine/Engine/Graphics/Drawables/Primitives/LinePrimitiveDrawable.cs
@@ -1,5 +1,21 @@
+using Furball.Engine.Engine.Graphics.Drawables.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
 namespace Furball.Engine.Engine.Graphics.Drawables.Primitives {
-    public class LinePrimitiveDrawable {
+    public class LinePrimitiveDrawable : ManagedDrawable {
+        public float Length;
+        public float Angle;
+        public float Thickness;
         
+        public LinePrimitiveDrawable(float length, float angle) {
+            this.Length = length;
+            this.Angle  = angle;
+        }
+        
+        public override void Draw(GameTime time, SpriteBatch batch, DrawableManagerArgs args) {
+            batch.DrawLine(args.Position, this.Length, this.Angle, args.Color, this.Thickness, args.LayerDepth);
+        }
     }
 }
---
+++ Furball.Engine/Engine/Graphics/Drawables/Primitives/RectanglePrimitiveDrawable.cs
@@ -0,0 +1,19 @@
+using Furball.Engine.Engine.Graphics.Drawables.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace Furball.Engine.Engine.Graphics.Drawables.Primitives {
+    public class RectanglePrimitiveDrawable : ManagedDrawable {
+        public bool    Filled;
+        public Vector2 RectSize;
+        public float   Thickness;
+        
+        public override void Draw(GameTime time, SpriteBatch batch, DrawableManagerArgs args) {
+            if(this.Filled)
+                batch.FillRectangle(args.Position, this.RectSize, args.Color, args.LayerDepth);
+            else
+                batch.DrawRectangle(args.Position, this.RectSize, args.Color, this.Thickness, args.LayerDepth);
+        }
+    }
+}
