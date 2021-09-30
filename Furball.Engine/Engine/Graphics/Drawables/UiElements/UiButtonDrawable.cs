42edfa2e70ff04aa8bfac16b030dbdec95eabba1
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-30 12:24:05 -0700

    UiElements: Fix rendering in Button when depth is not an int

--- Furball.Engine/Engine/Graphics/Drawables/UiElements/UiButtonDrawable.cs
+++ Furball.Engine/Engine/Graphics/Drawables/UiElements/UiButtonDrawable.cs
@@ -90,8 +90,8 @@ namespace Furball.Engine.Engine.Graphics.Drawables.UiElements {
         }
 
         public override void Draw(GameTime time, DrawableBatch batch, DrawableManagerArgs args) {
-            batch.SpriteBatch.FillRectangle(args.Position * FurballGame.VerticalRatio, this.Size * FurballGame.VerticalRatio, args.Color, args.LayerDepth);
-            batch.SpriteBatch.DrawRectangle(args.Position * FurballGame.VerticalRatio, this.Size * FurballGame.VerticalRatio, this.OutlineColor, this.OutlineThickness * FurballGame.VerticalRatio, args.LayerDepth);
+            batch.SpriteBatch.FillRectangle(args.Position * FurballGame.VerticalRatio, this.Size * FurballGame.VerticalRatio, args.Color, 0f);
+            batch.SpriteBatch.DrawRectangle(args.Position * FurballGame.VerticalRatio, this.Size * FurballGame.VerticalRatio, this.OutlineColor, this.OutlineThickness * FurballGame.VerticalRatio, 0f);
             
             // FIXME: this is a bit of a hack, it should definitely be done differently
             DrawableManagerArgs tempArgs = args;
