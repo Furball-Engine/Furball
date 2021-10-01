0d7eb72a1f80bfa36c052466ec38b5d16cc3c104
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-09-30 18:23:46 -0700

    Helpers: Implement Bindable<T> This allows you to bind to a value and recieve and event whenever it is changed

---
+++ Furball.Engine/Engine/Helpers/Bindable.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Furball.Engine.Engine.Helpers {
+    public class Bindable<T> : IDisposable {
+        public event EventHandler<T> OnChange;
+
+        private T _value;
+        public T Value {
+            get => this._value;
+            set {
+                if (this._value.Equals(value)) return;
+                
+                this._value = value;
+                this.OnChange?.Invoke(this, value);
+            }
+        }
+        
+        public Bindable(T value) {
+            this.Value = value;
+        }
+
+        public void Dispose() {
+            this.OnChange = null;
+        }
+        
+        public static implicit operator T(Bindable<T> bindable) {
+            return bindable.Value;
+        }
+    }
+}
