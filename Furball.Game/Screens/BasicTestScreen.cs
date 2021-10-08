2417d7f1056fa214622686811351d03c630d0123
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-10-07 20:58:12 -0700

    FurballGame: Change to initial screen only after the game has Initialized

--- Furball.Game/Screens/BasicTestScreen.cs
+++ Furball.Game/Screens/BasicTestScreen.cs
@@ -7,6 +7,8 @@ using Microsoft.Xna.Framework;
 namespace Furball.Game.Screens {
     public class BasicTestScreen : Screen {
         public override void Initialize() {
+            base.Initialize();
+            
             Random random = new Random();
             UiButtonDrawable screenSwitchButton = new UiButtonDrawable(new Vector2(random.Next(0, 1280), random.Next(0, 720)), "Test Switching Screen", FurballGame.DEFAULT_FONT, 30, Color.Cyan, Color.Red, Color.Black, new Vector2(200, 40));
 
@@ -15,8 +17,6 @@ namespace Furball.Game.Screens {
             };
 
             this.Manager.Add(screenSwitchButton);
-            
-            base.Initialize();
         }
     }
 }
