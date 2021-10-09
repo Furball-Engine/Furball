a256ae69e88a402a97a1d20469689522adc85b3c
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-10-09 11:49:24 -0700

    Localization: Add the rest of the enums and stuff and add 2 languages to get us started

---
+++ Furball.Engine/Engine/Localization/ISO639-2Scope.cs
@@ -0,0 +1,7 @@
+namespace Furball.Engine.Engine.Localization {
+    public enum ISO639_2Scope {
+        Individual,
+        Collective,
+        Macrolanguage,
+    }
+}
---
+++ Furball.Engine/Engine/Localization/ISO639-2Type.cs
@@ -0,0 +1,9 @@
+namespace Furball.Engine.Engine.Localization {
+    public enum ISO639_2Type {
+        Ancient,
+        Historical,
+        Extinct,
+        Constructed,
+        Living
+    }
+}
