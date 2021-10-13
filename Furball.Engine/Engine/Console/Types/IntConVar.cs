c2f89d4034721b2190428da4b85b304fca239ebe
Eevee <developerbrzoza@gmail.com>
2021-10-13 21:15:05 +0200

    ConVar: Change Type to Bindables

--- Furball.Engine/Engine/Console/Types/IntConVar.cs
+++ Furball.Engine/Engine/Console/Types/IntConVar.cs
@@ -1,11 +1,13 @@
+using Furball.Engine.Engine.Helpers;
+
 namespace Furball.Engine.Engine.Console.Types {
     public class IntConVar : ConVar {
-        public int Value { get; private set; }
+        public Bindable<int> Value;
 
         public IntConVar(string conVarName) : base(conVarName) {}
 
         public override void Set(string consoleInput) {
-            this.Value = int.Parse(consoleInput);
+            this.Value.Value = int.Parse(consoleInput);
         }
 
         public override string ToString() => this.Value.ToString();
--- Furball.Engine/Engine/Console/Types/StringConVar.cs
+++ Furball.Engine/Engine/Console/Types/StringConVar.cs
@@ -1,11 +1,13 @@
+using Furball.Engine.Engine.Helpers;
+
 namespace Furball.Engine.Engine.Console.Types {
     public class StringConVar : ConVar {
-        public string Value { get; private set; }
+        public Bindable<string> Value;
 
         public StringConVar(string conVarName) : base(conVarName) {}
 
         public override void Set(string consoleInput) {
-            this.Value = consoleInput;
+            this.Value.Value = consoleInput;
         }
 
         public override string ToString() => this.Value;
