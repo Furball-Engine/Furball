4118ccf1739ea823d0b46b9e8286b08d8f20ec59
Eevee <developerbrzoza@gmail.com>
2021-10-16 21:53:27 +0200

    DevConsoleLogger: fix compile error cuz tuple -> ConsoleResult

--- Furball.Engine/Engine/Helpers/Logger/DevConsoleLogger.cs
+++ Furball.Engine/Engine/Helpers/Logger/DevConsoleLogger.cs
@@ -3,7 +3,7 @@ using Furball.Engine.Engine.DevConsole;
 namespace Furball.Engine.Engine.Helpers.Logger {
     public class DevConsoleLogger : LoggerBase {
         public override void Send(LoggerLine line) {
-            DevConsole.DevConsole.ConsoleLog.Add((string.Empty, ExecutionResult.Log, line.LineData));
+            DevConsole.DevConsole.ConsoleLog.Add((string.Empty, new ConsoleResult(ExecutionResult.Log, line.LineData)));
         }
     }
 }
