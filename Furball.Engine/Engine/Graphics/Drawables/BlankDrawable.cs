a9f5ceabb981ae23bf25ff6c749c581d8791a58e
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-10-19 13:15:46 -0700

    Graphics: Add Blank and rewrite CompositeDrawable to work

---
+++ Furball.Engine/Engine/Graphics/Drawables/BlankDrawable.cs
@@ -0,0 +1,8 @@
+namespace Furball.Engine.Engine.Graphics.Drawables {
+    /// <summary>
+    /// Literally fucking nothing, used as a placeholder
+    /// </summary>
+    public class BlankDrawable : ManagedDrawable {
+        
+    }
+}
--- Furball.Engine/Engine/Graphics/Drawables/CompositeDrawable.cs
+++ Furball.Engine/Engine/Graphics/Drawables/CompositeDrawable.cs
@@ -1,61 +1,32 @@
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using Furball.Engine.Engine.Graphics.Drawables.Managers;
-using Microsoft.Xna.Framework;
 
 namespace Furball.Engine.Engine.Graphics.Drawables {
-    /// <summary>
-    /// CompositeDrawable allows for Multiple Drawables to be drawn in 1 drawable
-    /// It can also inherit values, so for example if CompositeDrawable is scaled to 2x, and the drawables are all 0.5x, theyll end up being 1x
-    /// It does the same to Position and Rotation (though only adding them up)
-    /// </summary>
     public class CompositeDrawable : ManagedDrawable {
-        private List<ManagedDrawable> _drawableList = new();
+        public List<ManagedDrawable> Drawables = new();
 
-        public bool InheritValues;
-        /// <summary>
-        /// Creates a CompositeDrawable
-        /// </summary>
-        /// <param name="position">Where to Draw</param>
-        /// <param name="inheritValues">Whether to Inherit Values or not</param>
-        public CompositeDrawable(Vector2 position, bool inheritValues = true) {
-            this.InheritValues = inheritValues;
-            this.Position      = position;
-            this.Clickable     = false;
+        public override Vector2 Size => new(0);
+
+        public override void Update(GameTime time) {
+            foreach (ManagedDrawable drawable in this.Drawables) {
+                drawable.Update(time);
+                drawable.UpdateTweens();
+            }
         }
 
         public override void Draw(GameTime time, DrawableBatch batch, DrawableManagerArgs args) {
-            int count = this._drawableList.Count;
-
-            if (count == 0)
-                return;
-
-            if (this.InheritValues) {
-                args.Position += this.Position;
-                args.Rotation += this.Rotation;
-                args.Scale    *= this.Scale;
-            }
-
-            for (int i = 0; i != count; i++) {
-                ManagedDrawable currentDrawable = this._drawableList[i];
-
-                currentDrawable.Draw(time, batch, args);
+            foreach (ManagedDrawable drawable in this.Drawables) {
+                DrawableManagerArgs drawableArgs = new() {
+                    Color    = drawable.ColorOverride,
+                    Effects  = args.Effects,
+                    Position = args.Position + drawable.Position,
+                    Rotation = args.Rotation,
+                    Scale    = args.Scale * drawable.Scale
+                };
+                
+                drawable.Draw(time, batch, drawableArgs);
             }
         }
-
-        public override void Update(GameTime time) {
-            int count = this._drawableList.Count;
-
-            if (count == 0)
-                return;
-
-            for (int i = 0; i != count; i++) {
-                ManagedDrawable currentDrawable = this._drawableList[i];
-
-                currentDrawable.Update(time);
-                currentDrawable.UpdateTweens();
-            }
-        }
-
-        public void Add(ManagedDrawable drawable) => this._drawableList.Add(drawable);
     }
 }
