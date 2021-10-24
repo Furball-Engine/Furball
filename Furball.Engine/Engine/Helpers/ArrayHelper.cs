3a28c8377205b4b107094f3230519892c54b0d3f
jiniux <alessandro.gugliotta.02@gmail.com>
2021-10-24 01:37:43 -0700

    Helpers: Add function to stretch an array into an array of a larger size

---
+++ Furball.Engine/Engine/Helpers/ArrayHelper.cs
@@ -0,0 +1,27 @@
+namespace Furball.Engine.Engine.Helpers {
+    public class ArrayHelper {
+        public static T[] FitElementsInANewArray <T>(T[] source, int targetLength) {
+            int sourceLength        = source.Length;
+            int baseSteps           = targetLength / sourceLength;
+            int surplusElementCount = targetLength % sourceLength;
+
+            T[] target = new T[targetLength];
+
+            int offset = 0;
+
+            for (int i = 0; i < sourceLength; i++) {
+                T element = source[i];
+
+                for (int j = 0; j < baseSteps; j++)
+                    target[offset + i * baseSteps + j] = element;
+
+                if (surplusElementCount > 0) {
+                    target[offset++ + baseSteps * (i + 1)] = element;
+                    surplusElementCount--;
+                }
+            }
+
+            return target;
+        }
+    }
+}
