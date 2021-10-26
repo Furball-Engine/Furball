ee6403e06ae6f470efdb19762419510453d80c11
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-10-26 13:32:35 -0700

    Graphics: Implement CatmullRom curves into CurveDrawable

--- Furball.Engine/Engine/Graphics/Drawables/CurveDrawable.cs
+++ Furball.Engine/Engine/Graphics/Drawables/CurveDrawable.cs
@@ -4,8 +4,8 @@ using Microsoft.Xna.Framework;
 using Xssp.MonoGame.Primitives2D;
 
 namespace Furball.Engine.Engine.Graphics.Drawables {
-    public class BezierCurveDrawable : ManagedDrawable {
-        public BezierCurveType Type;
+    public class CurveDrawable : ManagedDrawable {
+        public CurveType Type;
 
         public Bindable<Vector2> P0;
         public Bindable<Vector2> P1;
@@ -18,21 +18,21 @@ namespace Furball.Engine.Engine.Graphics.Drawables {
 
         public override Vector2 Size => new(100);
 
-        public BezierCurveDrawable(Vector2 p0, Vector2 p1, Vector2 p2) {
+        public CurveDrawable(Vector2 p0, Vector2 p1, Vector2 p2) {
             this.P0 = new Bindable<Vector2>(p0);
             this.P1 = new Bindable<Vector2>(p1);
             this.P2 = new Bindable<Vector2>(p2);
 
-            this.Type = BezierCurveType.Quadratic;
+            this.Type = CurveType.Quadratic;
         }
 
-        public BezierCurveDrawable(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3) {
+        public CurveDrawable(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3) {
             this.P0 = new Bindable<Vector2>(p0);
             this.P1 = new Bindable<Vector2>(p1);
             this.P2 = new Bindable<Vector2>(p2);
             this.P3 = new Bindable<Vector2>(p3);
 
-            this.Type = BezierCurveType.Cubic;
+            this.Type = CurveType.Cubic;
         }
 
         public override void Draw(GameTime time, DrawableBatch batch, DrawableManagerArgs args) {
@@ -41,23 +41,9 @@ namespace Furball.Engine.Engine.Graphics.Drawables {
                 float nextT = (float)(i + 1) / this.Quality;
 
                 switch (this.Type) {
-                    case BezierCurveType.Cubic: {
+                    case CurveType.Cubic: {
                         (float x, float y)   = BezierHelper.CubicBezier(this.P0, this.P1, this.P2, this.P3, t);
                         (float x1, float y1) = BezierHelper.CubicBezier(this.P0, this.P1, this.P2, this.P3, nextT);
-
-                        x *= FurballGame.VerticalRatio;
-                        y *= FurballGame.VerticalRatio;
-                        x1 *= FurballGame.VerticalRatio;
-                        y1 *= FurballGame.VerticalRatio;
-
-                        batch.SpriteBatch.DrawLine(x, y, x1, y1, args.Color, this.Thickness, 0);
-                        batch.SpriteBatch.DrawLine(x, y, x1, y1, args.Color, this.Thickness, 0);
-
-                        break;
-                    }
-                    case BezierCurveType.Quadratic: {
-                        (float x, float y)   = BezierHelper.QuadraticBezier(this.P0, this.P1, this.P2, t);
-                        (float x1, float y1) = BezierHelper.QuadraticBezier(this.P0, this.P1, this.P2, nextT);
 
                         x  *= FurballGame.VerticalRatio;
                         y  *= FurballGame.VerticalRatio;
@@ -69,8 +55,37 @@ namespace Furball.Engine.Engine.Graphics.Drawables {
 
                         break;
                     }
+                    case CurveType.Quadratic: {
+                        (float x, float y)   = BezierHelper.QuadraticBezier(this.P0, this.P1, this.P2, t);
+                        (float x1, float y1) = BezierHelper.QuadraticBezier(this.P0, this.P1, this.P2, nextT);
+
+                        x  *= FurballGame.VerticalRatio;
+                        y  *= FurballGame.VerticalRatio;
+                        x1 *= FurballGame.VerticalRatio;
+                        y1 *= FurballGame.VerticalRatio;
+
+                        batch.SpriteBatch.DrawLine(x, y, x1, y1, args.Color, this.Thickness, 0);
+                        batch.SpriteBatch.DrawLine(x, y, x1, y1, args.Color, this.Thickness, 0);
+                        
+                        break;
+                    }
+                    case CurveType.CatmullRom: {
+                        (float x, float y)   = Vector2.CatmullRom(this.P0, this.P1, this.P2, this.P3, t);
+                        (float x1, float y1) = Vector2.CatmullRom(this.P0, this.P1, this.P2, this.P3, nextT);
+
+                        batch.SpriteBatch.DrawLine(x, y, x1, y1, args.Color, this.Thickness, 0);
+                        batch.SpriteBatch.DrawLine(x, y, x1, y1, args.Color, this.Thickness, 0);
+
+                        break;
+                    }
                 }
             }
         }
+    }
+
+    public enum CurveType {
+        Quadratic,
+        Cubic,
+        CatmullRom
     }
 }
