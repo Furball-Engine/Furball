68d1b58df214ddb81fd958182706ee4b3634e0ad
Eevee <developerbrzoza@gmail.com>
2021-11-08 22:49:38 +0100

    Text Rendering Fuckage: scaling is fucked, batchedrenderer seems to have something fucked with textures

--- Furball.Engine/FurballGame.cs
+++ Furball.Engine/FurballGame.cs
@@ -61,7 +61,8 @@ namespace Furball.Engine {
         public static int WindowHeight => (int) Instance.WindowManager.WindowSize.Y;
 
         public static float HorizontalRatio => (float)WindowWidth / DEFAULT_WINDOW_WIDTH;
-        public static float VerticalRatio => (float)WindowHeight / DEFAULT_WINDOW_HEIGHT;
+        //public static float VerticalRatio => (float)WindowHeight / DEFAULT_WINDOW_HEIGHT;
+        public static float VerticalRatio => 1;
         public static Rectangle DisplayRect => new(0, 0, (int)Math.Ceiling(WindowWidth / VerticalRatio), (int)Math.Ceiling(WindowHeight / VerticalRatio));
         public static Rectangle DisplayRectActual => new(0, 0, WindowWidth, WindowHeight);
 
@@ -194,7 +195,7 @@ namespace Furball.Engine {
 
             ScreenManager.ChangeScreen(this._startScreen);
 
-            DrawableBatch = new DrawableBatch(RendererType.Batched);
+            DrawableBatch = new DrawableBatch(RendererType.Immediate);
 
             this.ChangeScreenSize(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
 
