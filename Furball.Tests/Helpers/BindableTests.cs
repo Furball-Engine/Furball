a5141b4f246b39d11dcd68870fb9d53b8ae45153
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-11-08 17:55:04 -0800

    Tests: Add full coverage unit tests on Bindable

---
+++ Furball.Tests/Helpers/BindableTests.cs
@@ -0,0 +1,85 @@
+using System;
+using Furball.Engine.Engine.Helpers;
+using Xunit;
+
+namespace Furball.Tests.Helpers {
+    public class BindableTests {
+        private const string STRING_START_VALUE = "begin";
+        private const string STRING_END_VALUE   = "end";
+
+        private const double DOUBLE_START_VALUE = Math.PI;
+
+        private Bindable<string> CreateStringBindable() {
+            Bindable<string> bindable = new(STRING_START_VALUE);
+
+            //Make sure that the bindable actually starts with its intended value
+            Assert.True(bindable.Value == STRING_START_VALUE, "The bindable did not start with the correct value!");
+
+            return bindable;
+        }
+
+        private Bindable<double> CreateDoubleBindable() {
+            Bindable<double> bindable = new(DOUBLE_START_VALUE);
+
+            //Make sure that the bindable actually starts with its intended value
+            Assert.True(bindable.Value == DOUBLE_START_VALUE, "The bindable did not start with the correct value!");
+
+            return bindable;
+        }
+
+        [Fact]
+        public void BindableTestOnChange() {
+            Bindable<string> bindable = this.CreateStringBindable();
+
+            bindable.OnChange += delegate(object _, string s) {
+                Assert.True(s == STRING_END_VALUE, "The value in the OnChange event is not what we want it to be!");
+            };
+
+            bindable.Value = STRING_END_VALUE;
+
+            Assert.True(bindable.Value == STRING_END_VALUE, "The value did not change to the intended value!");
+        }
+
+        [Fact]
+        public void BindableTestSameValueCheck() {
+            Bindable<string> bindable = this.CreateStringBindable();
+
+            bindable.OnChange += delegate {
+                Assert.True(false, "This should not be called the bindable does not change value!");
+            };
+
+            bindable.Value = STRING_START_VALUE;
+        }
+
+        [Fact]
+        public void BindableTestDispose() {
+            Bindable<string> bindable = this.CreateStringBindable();
+
+            bindable.OnChange += delegate {
+                Assert.True(false, "Bindable.OnChange should not be called after its disposed!");
+            };
+
+            bindable.Dispose();
+
+            bindable.Value = STRING_END_VALUE;
+        }
+
+        [Fact]
+        public void BindableTestToString() {
+            Bindable<double> bindable = this.CreateDoubleBindable();
+
+            Assert.True(bindable.ToString() == DOUBLE_START_VALUE.ToString(), "Bindable.ToString returned the wrong value!");
+        }
+
+        [Fact]
+        public void BindableTestImplicitConversion() {
+            Bindable<double> bindableDouble = this.CreateDoubleBindable();
+
+            Assert.True(bindableDouble == DOUBLE_START_VALUE, "The double implicit conversion failed!");
+
+            Bindable<string> bindableString = this.CreateStringBindable();
+
+            Assert.True(bindableString == STRING_START_VALUE, "The string implicit conversion failed!");
+        }
+    }
+}
