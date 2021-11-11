3e3e1dd12e94f33f930def74fe8db37270da5508
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-11-10 19:36:20 -0800

    Graphics: Add origin support for CompositeDrawable

--- Furball.Engine/Engine/Graphics/Drawables/CompositeDrawable.cs
+++ Furball.Engine/Engine/Graphics/Drawables/CompositeDrawable.cs
@@ -62,17 +62,21 @@ namespace Furball.Engine.Engine.Graphics.Drawables {
         }
 
         public override void Draw(GameTime time, DrawableBatch batch, DrawableManagerArgs args) {
-            foreach (ManagedDrawable drawable in this._drawables) {
-                if (!drawable.Visible) continue;
+            for (int i = 0; i < this._drawables.Count; i++) {
+                ManagedDrawable drawable = this._drawables[i];
                 
+                if (!drawable.Visible) continue;
+
+                drawable.LastCalculatedOrigin = drawable.CalculateOrigin();
+
                 DrawableManagerArgs drawableArgs = new() {
                     Color    = drawable.ColorOverride,
                     Effects  = args.Effects,
-                    Position = args.Position + (drawable.Position * args.Scale),
+                    Position = args.Position + (drawable.Position - drawable.LastCalculatedOrigin) * args.Scale,
                     Rotation = args.Rotation,
                     Scale    = args.Scale * drawable.Scale
                 };
-                
+
                 drawable.Draw(time, batch, drawableArgs);
             }
         }
