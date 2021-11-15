d7db6e5132205df9329b372b4497f86d6585214f
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-11-14 17:44:25 -0800

    UiElements: Add basic Dropdown drawable

---
+++ Furball.Engine/Engine/Graphics/Drawables/UiElements/UiDropdownDrawable.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using FontStashSharp;
+using Microsoft.Xna.Framework;
+
+namespace Furball.Engine.Engine.Graphics.Drawables.UiElements {
+    public class UiDropdownDrawable : CompositeDrawable {
+        public List<string> Items;
+        public Vector2      ButtonSize;
+        public FontSystem   Font;
+        public int          FontSize;
+
+        public bool Selected = false;
+
+        public string SelectedItem;
+
+        public UiDropdownDrawable(Vector2 position, List<string> items, Vector2 buttonSize, FontSystem font, int fontSize) {
+            this.Position   = position;
+            this.Items      = items;
+            this.ButtonSize = buttonSize;
+            this.Font       = font;
+            this.FontSize   = fontSize;
+
+            this.SelectedItem = items[0];
+
+            this.Update();
+        }
+
+        public void Update() {
+            this._drawables.Clear();
+
+            if (this.Selected) {
+                UiButtonDrawable element = new(new(0, 0), this.SelectedItem, this.Font, this.FontSize, Color.Blue, Color.White, Color.Black, this.ButtonSize);
+
+                element.OnClick += delegate {
+                    this.Selected = false;
+
+                    this.Update();
+                };
+
+                this._drawables.Add(element);
+
+                float y = element.Size.Y;
+                foreach (string item in this.Items) {
+                    if (item == this.SelectedItem) continue;
+
+                    element = new(new(0, y), item, this.Font, this.FontSize, Color.Blue, Color.White, Color.Black, this.ButtonSize);
+
+                    element.OnClick += delegate {
+                        this.SelectedItem = item;
+
+                        this.Selected = false;
+
+                        this.Update();
+                    };
+
+                    this._drawables.Add(element);
+
+                    y += element.Size.Y;
+                }
+            } else {
+                UiButtonDrawable element = new(new(0, 0), this.SelectedItem, this.Font, this.FontSize, Color.Blue, Color.White, Color.Black, this.ButtonSize);
+
+                element.OnClick += delegate {
+                    this.Selected = true;
+
+                    this.Update();
+                };
+
+                this._drawables.Add(element);
+            }
+        }
+    }
+}
