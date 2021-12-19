31ea165faf939c4a7d6718ee670853da5eb59800
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-12-19 13:47:42 -0800

    Graphics: Re-add CirclePrimitiveDrawable This currently does not look the best because it is not a shader rather it is just some antialiased lines, but at least it exists now.

--- Furball.Engine/Engine/Graphics/Drawables/Primitives/CirclePrimitiveDrawable.cs
+++ Furball.Engine/Engine/Graphics/Drawables/Primitives/CirclePrimitiveDrawable.cs
@@ -1,33 +1,64 @@
-//TODO(Eevee)@Vixie: circle drawing shader stuff thing ass
-//using Furball.Engine.Engine.Graphics.Drawables.Managers;
-//
-//namespace Furball.Engine.Engine.Graphics.Drawables.Primitives {
-//    /// <summary>
-//    /// Simple Circle Drawable
-//    /// </summary>
-//    public class CirclePrimitiveDrawable : ManagedDrawable {
-//        /// <summary>
-//        ///     The detail of the circle
-//        /// </summary>
-//        public int Detail;
-//        /// <summary>
-//        /// Creates a Circle
-//        /// </summary>
-//        /// <param name="position">Where to Draw</param>
-//        /// <param name="radius">How big should the Circle be</param>
-//        /// <param name="outlineColor">What Color should the Border be</param>
-//        /// <param name="detail">How many sides are on the circle</param>
-//        public CirclePrimitiveDrawable(Vector2 position, float radius, Color outlineColor, int detail = 25) {
-//            this.CircleRadius  = radius;
-//            this.Circular      = true;
-//            this.ColorOverride = outlineColor;
-//            this.Position      = position;
-//            this.Detail        = detail;
-//        }
-//
-//        public override void Draw(double time, DrawableBatch batch, DrawableManagerArgs args) {
-//            batch.SpriteBatch.DrawCircle(args.Position * FurballGame.VerticalRatio, this.CircleRadius * FurballGame.VerticalRatio, this.Detail, args.Color, 0f);
-//        }
-//    }
-//}
-//
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Furball.Engine.Engine.Graphics.Drawables.Managers;
+using Furball.Engine.Engine.Helpers;
+using Furball.Vixie.Graphics;
+
+namespace Furball.Engine.Engine.Graphics.Drawables.Primitives {
+    /// <summary>
+    /// Simple Circle Drawable
+    /// </summary>
+    public class CirclePrimitiveDrawable : ManagedDrawable {
+        /// <summary>
+        ///     The detail of the circle
+        /// </summary>
+        public Bindable<int> Detail;
+
+        public Vector2[] _calculatedPoints;
+
+        public float Thickness;
+
+        public void Recalculate() {
+            this._calculatedPoints = new Vector2[this.Detail];
+            
+            for (int i = 0; i < this.Detail; i++) {    
+                double angle = 2 * Math.PI *i / this.Detail; 
+                this._calculatedPoints[i] = new((float)(Math.Cos(angle) * this.CircleRadius), (float)(Math.Sin(angle) * this.CircleRadius)); 
+            } 
+        }
+
+        /// <summary>
+        /// Creates a Circle
+        /// </summary>
+        /// <param name="position">Where to Draw</param>
+        /// <param name="radius">How big should the Circle be</param>
+        /// <param name="outlineColor">What Color should the Border be</param>
+        /// <param name="thickness">The thickness of the outline</param>
+        /// <param name="detail">How many sides are on the circle</param>
+        public CirclePrimitiveDrawable(Vector2 position, float radius, Color outlineColor, float thickness, int detail = 25) {
+            this.CircleRadius  = new(radius);
+            this.Circular      = true;
+            this.ColorOverride = outlineColor;
+            this.Position      = position;
+            this.Detail        = new(detail);
+            this.Thickness     = thickness;
+            
+            this.Recalculate();
+
+            this.Detail.OnChange        += (_, _) => this.Recalculate();
+            this.CircleRadius!.OnChange += (_, _) => this.Recalculate();
+        }
+
+        public override void Draw(double time, DrawableBatch batch, DrawableManagerArgs args) {
+            for (int i = 0; i < this._calculatedPoints.Length; i++) {
+                batch.DrawLine(
+                this._calculatedPoints[i] + this.Position,
+                (i == this._calculatedPoints.Length - 1 ? this._calculatedPoints[0] : this._calculatedPoints[i + 1]) + this.Position,
+                this.Thickness,
+                this.ColorOverride
+                );
+            }
+        }
+    }
+}
