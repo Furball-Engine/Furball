77e113d5e188535ec0e7e6bf06290f6114eb0fd2
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-05-07 09:30:00 -0700

    Config: Add easy getters onto config

--- Furball.Engine/Engine/Config/FurballConfig.cs
+++ Furball.Engine/Engine/Config/FurballConfig.cs
@@ -4,8 +4,17 @@ namespace Furball.Engine.Engine.Config {
     public class FurballConfig : VolpeConfig {
         public override string Name => "furball";
 
+        public int ScreenWidth  => (int)this.Values["screen_width"].ToNumber().Value;
+        public int ScreenHeight => (int)this.Values["screen_height"].ToNumber().Value;
+
+
+        public bool LimitFPS  => this.Values["limit_fps"].ToBoolean().Value;
+        public int  TargetFPS => (int)this.Values["target_fps"].ToNumber().Value;
+
+        public bool Fullscreen => this.Values["fullscreen"].ToBoolean().Value;
+        
         public FurballConfig() {
-            this.Values["screen_width"] = new Value.Number(FurballGame.DEFAULT_WINDOW_WIDTH);
+            this.Values["screen_width"]  = new Value.Number(FurballGame.DEFAULT_WINDOW_WIDTH);
             this.Values["screen_height"] = new Value.Number(FurballGame.DEFAULT_WINDOW_HEIGHT);
 
             this.Values["limit_fps"]  = new Value.Boolean(true);
