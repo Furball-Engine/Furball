7708a4275489edab2bfd580da27e28c9eb46aa20
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-06-14 10:40:19 -0700

    Add GL and GLES versions to backend display

--- Furball.Engine/Engine/Debug/DebugCounter/Items/GraphicsBackend.cs
+++ Furball.Engine/Engine/Debug/DebugCounter/Items/GraphicsBackend.cs
@@ -1,13 +1,38 @@
+using Furball.Vixie.Backends.OpenGL20;
+using Furball.Vixie.Backends.OpenGL41;
+using Furball.Vixie.Backends.OpenGLES;
 using Furball.Vixie.Backends.Veldrid;
 
 namespace Furball.Engine.Engine.Debug.DebugCounter.Items {
     public class GraphicsBackend : DebugCounterItem {
-        public override string GetAsString(double time) {
+        private string str;
+        
+        public GraphicsBackend() {
             if (Vixie.GraphicsBackend.Current is VeldridBackend veldridBackend) {
-                return $"Backend: Veldrid ({veldridBackend.ChosenBackend})";
+                str = $"Backend: Veldrid ({veldridBackend.ChosenBackend})";
+                return;
+            }
+
+            if (Vixie.GraphicsBackend.Current is OpenGL41Backend openGl41Backend) {
+                str = $"Backend: ModernOpenGL ({Vixie.Backends.Shared.Global.LatestSupportedGL.GL.MajorVersion}.{Vixie.Backends.Shared.Global.LatestSupportedGL.GL.MinorVersion})";
+                return;
             }
             
-            return $"Backend: {FurballGame.Instance.WindowManager.Backend}";
+            if (Vixie.GraphicsBackend.Current is OpenGL20Backend openGl20Backend) {
+                str = $"Backend: LegacyOpenGL ({Vixie.Backends.Shared.Global.LatestSupportedGL.GL.MajorVersion}.{Vixie.Backends.Shared.Global.LatestSupportedGL.GL.MinorVersion})";
+                return;
+            }
+            
+            if (Vixie.GraphicsBackend.Current is OpenGLESBackend openGlesBackend) {
+                str = $"Backend: OpenGLES ({Vixie.Backends.Shared.Global.LatestSupportedGL.GLES.MajorVersion}.{Vixie.Backends.Shared.Global.LatestSupportedGL.GLES.MinorVersion})";
+                return;
+            }
+
+            str = $"Backend: {FurballGame.Instance.WindowManager.Backend}";
+        }
+        
+        public override string GetAsString(double time) {
+            return str;
         }
     }
 }
