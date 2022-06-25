fe24e5dcd69c3574485c88f82b0507350f09c5dc
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-06-25 15:34:42 -0700

    Graphics: Relicense VideoDecoder.cs under MPL

--- Furball.Engine/Engine/Graphics/VideoDecoder.cs
+++ Furball.Engine/Engine/Graphics/VideoDecoder.cs
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Originally written by Beyley Thomas <ep1cm1n10n123@gmail.com>
+ */
+
 using System;
 using System.Collections.Generic;
 using System.IO;
