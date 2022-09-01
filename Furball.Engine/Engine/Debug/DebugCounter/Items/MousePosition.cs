fd477e743b0ceb1f9c7c56d0ea40b999644ffb22
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-08-31 21:13:25 -0700

    DebugCounter: Handle zero or more than one mouse cursors

--- Furball.Engine/Engine/Debug/DebugCounter/Items/MousePosition.cs
+++ Furball.Engine/Engine/Debug/DebugCounter/Items/MousePosition.cs
@@ -1,8 +1,20 @@
+using System.Text;
+using Furball.Engine.Engine.Input;
+
 namespace Furball.Engine.Engine.Debug.DebugCounter.Items; 
 
 /// <summary>
 /// Displays the Desktop Mouse Position
 /// </summary>
 public class MousePosition : DebugCounterItem {
-    public override string GetAsString(double time) => $"mouse: ({FurballGame.InputManager.CursorStates[0].Position.X:N2}, {FurballGame.InputManager.CursorStates[0].Position.Y:N2})";
+    public override string GetAsString(double time) {
+        StringBuilder builder = new("mouse: ");
+
+        for (int i = 0; i < FurballGame.InputManager.CursorStates.Count; i++) {
+            FurballMouseState cursor = FurballGame.InputManager.CursorStates[i];
+            builder.Append($"[{i}]({cursor.Position.X}x{cursor.Position.Y}, {cursor.Name}, {string.Join(",", cursor.PressedButtons)}, {cursor.ScrollWheel.X}x{cursor.ScrollWheel.Y}) ");
+        }
+
+        return builder.ToString().Trim();
+    }
 }
