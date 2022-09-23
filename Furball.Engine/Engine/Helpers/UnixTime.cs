7909a76e775686228290a460501b833d0ac43943
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-09-22 23:26:46 -0700

    Helpers: Simplify UnixTime getter function

--- Furball.Engine/Engine/Helpers/UnixTime.cs
+++ Furball.Engine/Engine/Helpers/UnixTime.cs
@@ -7,7 +7,5 @@ public static class UnixTime {
     /// Easier Way to Get the Unix Time Quickly
     /// </summary>
     /// <returns>Unix time Right now</returns>
-    public static long Now() {
-        return ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
-    }
+    public static long Now() => DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 }
