4d197f8c7e80a815bf8f8fa574ab02efa5f3fac2
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-13 23:11:53 -0700

    DebugCounter: Fix high memory usage ironically by the memory usage counter

--- Furball.Engine/Engine/Debug/DebugCounter/Items/MemoryUsage.cs
+++ Furball.Engine/Engine/Debug/DebugCounter/Items/MemoryUsage.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 namespace Furball.Engine.Engine.Debug.DebugCounter.Items;
 
 public class MemoryUsage : DebugCounterItem {
-    private readonly FixedTimeStepMethod _updateTimeStep;
-
     private long _managedMemory = GC.GetTotalMemory(false);
     private long _unmanagedMemory;
 
@@ -15,22 +13,18 @@ public class MemoryUsage : DebugCounterItem {
         this._process = Process.GetCurrentProcess();
     }
 
-    public override void Update(double time) {
-        base.Update(time);
-
-        this._process.Refresh();
-
-        this._managedMemory   = GC.GetTotalMemory(true);
-        this._unmanagedMemory = this._process.PrivateMemorySize64 - this._managedMemory;
-    }
-
     public override void Dispose() {
         base.Dispose();
 
         this._process.Dispose();
-        
-        FurballGame.TimeStepMethods.Remove(this._updateTimeStep);
     }
 
-    public override string GetAsString(double time) => $"mmu: {this._managedMemory / 1024f:N2}kb umu: {this._unmanagedMemory / 1024f:N2}kb";
+    public override string GetAsString(double time) {
+        this._process.Refresh();
+
+        this._managedMemory   = GC.GetTotalMemory(true);
+        this._unmanagedMemory = this._process.PrivateMemorySize64 - this._managedMemory;
+
+        return $"mmu: {this._managedMemory / 1024f:N2}kb umu: {this._unmanagedMemory / 1024f:N2}kb";
+    }
 }
